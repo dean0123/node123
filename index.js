ff028fd4973229b33eef58abbb16412bb0e6e37e
dean0123 <dean0224@gmail.com>
2021-07-21 18:10:38 +0800

    a

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var server = require("./server");
+//var server = require("./server");  // 呼叫 自己寫的 server.js 物件
+//server.start();
 
-server.start();
+var express = require('express'); 
+var app = express();  // 產生 Express app 物件 
+ 
+app.get('/', function (req, res) {  // req 是用戶請求, res 是回應
+    res.send('Hello World Express');
+    console.log("->", req.url)
+ })
+ app.get('/blog', function (req, res) {  // 重訂路徑 不想讓用戶使用, 
+    res.redirect('/');
+    console.log("->", req.url)
+ })
+ app.get('/json', function (req, res) {  // 返回JSON 格式, 給 API 的呼叫 
+    res.json({
+        result: "ok"
+    }
+
+    );
+    console.log("->", req.url)
+ })
+  
+app.get('/setup', function (req, res) {    // 其他路徑 回應 
+    res.send('This is Setup  Paths');
+ })
+ 
+
+app.listen(8888, function () {
+   console.log("Example app listening at http://localhost:8888")
+})
