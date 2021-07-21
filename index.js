036c7c604f9405f68cfbf595ee09d56edfa88130
dean0123 <dean0224@gmail.com>
2021-07-21 18:40:22 +0800

    b

--- index.js
+++ index.js
@@ -14,13 +14,18 @@ app.get('/', function (req, res) {  // req 是用戶請求, res 是回應
  })
  app.get('/json', function (req, res) {  // 返回JSON 格式, 給 API 的呼叫 
     res.json({
-        result: "ok"
-    }
-
-    );
+        result_json: "okss"
+    });
     console.log("->", req.url)
  })
   
+ app.get('/login', function (req, res) {   
+    res.json({
+        Post_result: "OK2"
+    });
+    console.log(req.body)
+ })
+
 app.get('/setup', function (req, res) {    // 其他路徑 回應 
     res.send('This is Setup  Paths');
  })
