663aea11919f81e55510156d0d8f1ec414263940
dean0123 <dean0224@gmail.com>
2021-07-22 14:30:00 +0800

    a

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 //var server = require("./server");  // 呼叫 自己寫的 server.js 物件
 //server.start();
 
-var express = require('express'); 
-var app = express();  // 產生 Express app 物件 
+const express = require('express'); 
+const app = express();  // 產生 Express app 物件 
  
-app.get('/', function (req, res) {  // req 是用戶請求, res 是回應
-    res.send('Hello World Express');
+// ==== 1 GET : HELLO WORL  =====
+app.get('/',  (req, res)=>{  // req 是用戶請求, res 是回應
+    res.send('Hello World Express 4');
     console.log("->", req.url)
  })
- app.get('/blog', function (req, res) {  // 重訂路徑 不想讓用戶使用, 
-    res.redirect('/');
-    console.log("->", req.url)
- })
- app.get('/json', function (req, res) {  // 返回JSON 格式, 給 API 的呼叫 
+
+// === 2. SHOW JSON ====
+app.get('/json', function (req, res) {  // 返回JSON 格式, 給 API 的呼叫 
     res.json({
         result_json: "okss"
     });
     console.log("->", req.url)
- })
+})
   
- app.get('/login', function (req, res) {   
-    res.json({
-        Post_result: "OK2"
-    });
-    console.log(req.body)
- })
+// ==== 3. POST ====
 
-app.get('/setup', function (req, res) {    // 其他路徑 回應 
-    res.send('This is Setup  Paths');
- })
- 
+// For Express version less than 4.16.0   Depredecaded
+// ------------------------------------
+//const bodyParser = require('body-parser');
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+// May NOT need to NPM INSTALL BODY-PARSER 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.post('/login', (req, res)=>{   
+    res.json({
+        Post_result: "OK4",
+        req_body: req.body
+    });
+   console.log("Got POST request and response --->",req.body);   
+})
+
+// Static files like HTML. CSS, JPG, JS. ...
+app.use(express.static('public'))
 
 app.listen(8888, function () {
    console.log("Example app listening at http://localhost:8888")
